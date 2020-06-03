93b580486016201f869e0834ed0cacfcd001adf4
kingkingburger <dnjsalsgh123@gmail.com>
2020-06-03 19:25:44 +0900

    adding files

---
+++ Kartrider/Assets/Scenes/Basic_Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic_Move : MonoBehaviour
+{
+    public int Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+
+    }
+}
